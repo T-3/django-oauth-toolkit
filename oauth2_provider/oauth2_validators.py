f95769d3051fca7758f023b00547978216f87ca8
Bryan Rehbein <bryan.rehbein@t-3.com>
2013-12-10 17:13:22 -0600

    allow client credentials

--- oauth2_provider/oauth2_validators.py
+++ oauth2_provider/oauth2_validators.py
@@ -17,8 +17,8 @@ Application = get_application_model()
 log = logging.getLogger('oauth2_provider')
 
 GRANT_TYPE_MAPPING = {
-    'authorization_code': (Application.GRANT_AUTHORIZATION_CODE,),
-    'password': (Application.GRANT_PASSWORD,),
+    'authorization_code': (Application.GRANT_AUTHORIZATION_CODE, Application.GRANT_CLIENT_CREDENTIALS,),
+    'password': (Application.GRANT_PASSWORD, Application.GRANT_CLIENT_CREDENTIALS,),
     'client_credentials': (Application.GRANT_CLIENT_CREDENTIALS,),
     'refresh_token': (Application.GRANT_AUTHORIZATION_CODE, Application.GRANT_PASSWORD,
                       Application.GRANT_CLIENT_CREDENTIALS)
